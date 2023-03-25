f131ab24491251295ce89a543cdfb2ac48870199
chaitanyak175 <karmalkarchaitanya@gmail.com>
2023-03-25 17:57:39 +0530

    Email verification

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:test/login.dart';
+
+
+void main() => runApp(
+  const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  )
+);
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.all(20),
+        color: const Color(0xff8186F0),
+        child: const Center(child: LoginPage()),
+      ),
+    );
+  }
+}
